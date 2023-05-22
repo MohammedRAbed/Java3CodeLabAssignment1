a8d62d963bd34bbd80353e336251ca516fa30288
MoAbed <abedmohammed2004@gmail.com>
2023-05-22 06:39:04 +0300

    Second Edition

--- src/main/java/com/example/a1project/Model/TestModel.java
+++ src/main/java/com/example/a1project/Model/TestModel.java
@@ -2,6 +2,6 @@ package com.example.a1project.Model;
 
 public class TestModel {
     public TestModel() {
-        System.out.println("Hello World");
+        System.out.println("Hello World, this is test model for uploading project to my GitHub");
     }
 }
